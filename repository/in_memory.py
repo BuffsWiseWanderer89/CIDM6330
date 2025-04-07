d07df7a757e8ab5faf370374d8b71193a2383569
Chris Gariby <cgariby1@buffs.wtamu.edu>
2025-04-06 23:35:11 -0500

    Updated all 'id' variables to obj_id

--- repository/in_memory.py
+++ repository/in_memory.py
@@ -8,19 +8,18 @@ class InMemoryRepository(Repository):
         self.storage.append(obj)
         return obj
     
-    def read(self, id):
-        return next((item for item in self.storage if item.id == id), None)
+    def read(self, object_id):
+        return next((item for item in self.storage if item.object_id == object_id), None)
 
-    def update(self, id, obj):
-        for i in item in enumerate(self.storage):
-            if i.id == id:
-                self.storage[i] = obj
+    def update(self, object_id, obj):
+        for index, item in enumerate(self.storage):
+            if item.object_id == object_id:
+                self.storage[index] = obj
                 return obj
         return None
     
-    def delete(self, id):
-        self.storage = [item for item in self.storage if item.id != id]
+    def delete(self, object_id):
+        self.storage = [item for item in self.storage if item.id != object_id]
     
     def list_all(self):
         return self.storage
-    
